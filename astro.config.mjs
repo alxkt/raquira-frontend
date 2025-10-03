735fd322804054f3518ea4354326264582723102
Alex Turnbull <alexkturnbull@gmail.com>
2025-10-03 15:07:29 -0700

    Change to SSR temporarily to refetch on reload

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,10 @@
 import { defineConfig } from 'astro/config';
+import node from '@astrojs/node';
 import tailwindcss from '@tailwindcss/vite';
-// https://astro.build/config
+
+// SSR temporarily enabled for dynamic random gallery; remove adapter & prerender flag to revert to full SSG.
 export default defineConfig({
+  adapter: node({ mode: 'standalone' }),
   vite: {
     plugins: [tailwindcss()]
   }
